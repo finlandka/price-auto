5a9fb1467c4dabb05ceeda69af864e65d43f4947
Lena <finlandka@yandex.ru>
2024-06-30 12:50:42 +0300

    test yandex smtp

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -3,15 +3,12 @@ const { body, validationResult } = require('express-validator');
 //const axios = require('axios');
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.jino.ru',
+    host: 'smtp.yandex.ru.',
     port: 465,
     secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    tls: {
-        rejectUnauthorized: false
     }
 });
 
