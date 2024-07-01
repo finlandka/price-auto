7aaf47685fca5044e3a658be8be07011973f5936
Lena <finlandka@yandex.ru>
2024-07-01 15:57:50 +0300

    smtp

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -3,7 +3,7 @@ const { body, validationResult } = require('express-validator');
 const axios = require('axios');
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.yandex.ru.',
+    host: 'smtp.yandex.ru',
     port: 465,
     secure: true,
     auth: {
